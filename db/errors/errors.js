889a11b6ae53550fc02f0c99be2636c6c4e4fff8
Matt <cunning.distraction@googlemail.com>
2022-03-24 01:36:27 +0000

    updated README.md

--- db/errors/errors.js
+++ db/errors/errors.js
@@ -1,13 +1,10 @@
 // handles 404 errors for invalid routes
 exports.handle404 = (req, res) => {
-  console.log("handling 404 error");
   res.status(404).send({ message: "URL not found" });
 };
 
 // handles errors returned from PSQL database
 exports.handlePsqlErrors = (err, req, res, next) => {
-  console.log("handling psql error");
-  // console.log(err.code, "error code");
   if (err.code === "22P02" || err.code === "23502") {
     res.status(400).send({ message: "Bad request" });
   }
@@ -17,6 +14,5 @@ exports.handlePsqlErrors = (err, req, res, next) => {
 
 // handles anything passed to it by Promise.reject
 exports.handleCustomErrors = (err, req, res, next) => {
-  console.log("handling custom error");
   res.status(err.status).send({ message: err.message });
 };
