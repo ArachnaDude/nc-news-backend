96c14f9c9e446c6cfca1f2fa2027d97caff4e778
Matt <cunning.distraction@googlemail.com>
2022-05-05 23:00:12 +0100

    remove console logs

--- db/controllers/fetchCommentsByArticle.controllers.js
+++ db/controllers/fetchCommentsByArticle.controllers.js
@@ -3,10 +3,8 @@ const {
 } = require("../models/getCommentsByArticle.models");
 
 exports.fetchCommentsByArticle = (req, res, next) => {
-  console.log("controller");
   const { article_id } = req.params;
   const { sort_by, order } = req.query;
-  console.log(sort_by, order);
   getCommentsByArticle(article_id, sort_by, order)
     .then((comments) => {
       res.status(200).send({ comments });
